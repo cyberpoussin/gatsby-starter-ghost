922a15cdd53ffecb27b04d023e62a840848a2c9e
cyberpoussin <58190581+cyberpoussin@users.noreply.github.com>
2021-01-05 22:18:19 +0100

    Update script.js

--- src/templates/script.js
+++ src/templates/script.js
@@ -1,15 +1,26 @@
  if(typeof window !== 'undefined' && window.document) {
                     
 let scrollpos = window.scrollY
-                    const header = document.querySelector(".site-post-title")
-                    const header_height = header.offsetHeight
-                    const add_class_on_scroll = () => header.classList.add("fade-in")
-                    const remove_class_on_scroll = () => header.classList.remove("fade-in")
+                    const title = document.querySelector(".site-post-title")
+                    const menu = document.querySelector(".site-nav-menu")
+
+                    const header_height = title.offsetHeight
+                    const add_class_on_scroll = () => title.classList.add("fade-in")
+                    const add_other_class_on_scroll = () => menu.classList.add("fade-out")
+
+                    const remove_class_on_scroll = () => title.classList.remove("fade-in")
+                    const remove_other_class_on_scroll = () => menu.classList.remove("fade-out")
+
                     window.addEventListener('scroll', function() { 
                     scrollpos = window.scrollY;
-                    if (scrollpos >= header_height) { add_class_on_scroll() }
-                    else { remove_class_on_scroll() }
-                    console.log(scrollpos)
+                    if (scrollpos >= header_height) { 
+                        add_class_on_scroll() 
+                        add_other_class_on_scroll()
+                    }
+                    else { 
+                        remove_class_on_scroll() 
+                        remove_other_class_on_scroll() 
+                    }
                     })
      
  }
